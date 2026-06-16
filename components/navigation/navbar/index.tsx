209945cc89c49499062050631f5227084df520da
Nischal Puri <mrnischallpuri@gmail.com>
2026-06-16 16:52:27 +0545

    Project Setup and nav component created

---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import Link from "next/link"
+import { Theme } from "./theme"
+
+const NavBar = () => {
+  return (
+    <div className="flex-between background-light900_dark200 fixed z-50 w-full  p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex-center gap-1 ">
+        <Image
+          src="/images/site-logo.svg"
+          alt="Dev Overflow Logo"
+          width={30}
+          height={30}
+        />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">Overflow</span>
+        </p>
+      </Link>
+
+      <p>Global Search</p>
+
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
