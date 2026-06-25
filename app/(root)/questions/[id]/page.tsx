763609cf7c963803e7a3862c651428f7086b9c13
Nischal Puri <mrnischallpuri@gmail.com>
2026-06-25 15:14:23 +0545

    display answer to question detail page

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -10,6 +10,8 @@ import { redirect } from "next/navigation"
 import View from "../View"
 import { after } from "next/server"
 import AnswerForm from "@/components/forms/AnswerForm"
+import { getAnswers } from "@/lib/actions/answer.action"
+import AllAnswers from "@/components/answers/AllAnswers"
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params
@@ -17,6 +19,18 @@ const QuestionDetails = async ({ params }: RouteParams) => {
 
   if (!success || !question) return redirect("/404")
 
+  const {
+    success: areAnswersLoaded,
+    data: answersResult,
+    error: answersError,
+  } = await getAnswers({
+    questionId: id,
+    page: 1,
+    pageSize: 10,
+    filter: "latest",
+  })
+
+  console.log("Anwers", answersResult)
   const { author, createdAt, answers, views, tags, content, title } = question
   after(async () => {
     await incrementViews({ questionId: id })
@@ -90,6 +104,16 @@ const QuestionDetails = async ({ params }: RouteParams) => {
           />
         ))}
       </div>
+
+      <section className="my-5">
+        <AllAnswers
+          data={answersResult?.answers}
+          success={areAnswersLoaded}
+          error={answersError}
+          totalAnswers={answersResult?.totalAnswers || 0}
+        />
+      </section>
+
       <section className="my-5">
         <AnswerForm
           questionId={id}
